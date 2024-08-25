75b58d983088ca4e90203e7e501a1d4048b73874
het patel <hppatidar12d@gmail.com>
2024-08-25 14:23:34 +0530

    first commit

---
+++ generate.py
@@ -0,0 +1,29 @@
+import torch
+from diffusers import DiffusionPipeline, StableDiffusionXLImg2ImgPipeline
+import os
+
+prompt = 'professional headshot with a professional hairstyle and wearing a casual cloths with little smile'
+
+prj_path = "username/repo_name"
+model = os.environ.get('MODEL_NAME')
+pipe = DiffusionPipeline.from_pretrained(
+    model,
+    torch_dtype=torch.float16,
+)
+pipe.enable_model_cpu_offload()
+pipe.load_lora_weights(
+    prj_path, weight_name="pytorch_lora_weights.safetensors")
+
+refiner = StableDiffusionXLImg2ImgPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-refiner-1.0",
+    torch_dtype=torch.float16,
+)
+refiner.to("cuda")
+
+prompt = "generate a professional headshot with a professional hairstyle and wearing a casual cloths with little smile"
+
+seed = 42
+generator = torch.Generator("cuda").manual_seed(seed)
+image = pipe(prompt=prompt, generator=generator).images[0]
+image = refiner(prompt=prompt, generator=generator, image=image).images[0]
+image.save(f"generated_image.png")
